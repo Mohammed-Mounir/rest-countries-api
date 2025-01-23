9861c7ecba1b14e267020c4d8321895bb2c9a0c6
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2025-01-23 02:16:31 +0200

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListComponent } from './countries/list/list.component';
-import { DetailsComponent } from './countries/details/details.component';
-
-export const routes: Routes = [
-  { path: '', title: 'Countries', component: ListComponent },
-  { path: 'details/:country', title: 'Country', component: DetailsComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },
-];
